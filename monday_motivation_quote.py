3df04d079683895c0c82ee7f7ac14d50f207e4a3
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-06-30 16:18:48 +0300

    Add files via upload

---
+++ monday_motivation_quote.py
@@ -0,0 +1,22 @@
+import datetime
+import random
+import getpass
+import smtplib
+today = datetime.datetime.now()
+year = today.year
+day_of_week = today.weekday()
+if day_of_week == 3:
+    with open("quotes.txt") as file:
+        read = file.readlines()
+        random_quote = random.choice(read)
+    email = "your email"
+    #get your password from password for apps inside gmail/yahoo account security settings
+    password_email = getpass.getpass("Password: ")
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        #secures connection
+        connection.starttls()
+        connection.login(user=email, password=password_email)
+        subject = "Monday Motivation"
+        message = random_quote
+        email_body = "Subject: " + subject + '\n' + message
+        connection.sendmail(from_addr=email, to_addrs=email, msg=email_body)
